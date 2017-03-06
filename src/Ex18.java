c39e705f8d5f9503a2ff2d58965d36c5662647d2
wangc <wangc@chinalof.com.cn>
2017-03-06 10:47:14 +0800

    add drawSort

--- src/Ex18.java
+++ src/Ex18.java
@@ -1,4 +1,4 @@
-
+import edu.princeton.cs.algs4.In;
 
 /**
  * Á·Ï°2.1.18
@@ -8,7 +8,13 @@
 public class Ex18 {
 
 	public static void main(String[] args) {
-
+		String[] a = In.readStrings("re/tiny.txt");
+		String sort = "insertion";
+		if (sort.equals("selection")) {
+			Selection.drawSort(a);
+		} else if (sort.equals("insertion")) {
+			Insertion.drawSort(a);
+		}
 	}
 
 }
